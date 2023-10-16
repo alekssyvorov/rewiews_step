ee87239e41bb5caf711f54a3d4994f640118901e
Djekson <alekssyvorov29@gmail.com>
2023-10-16 10:06:32 +0300

    111

--- logopass.py
+++ logopass.py
@@ -1,11 +1,5 @@
-# login_user = 'keba_e'
-# password_user = '9uN8meT8116H'
-login_user = 'keba_i'
-password_user = 'wE7TkgV9892N'
-#kabal_m
-# password_user = 'Mihailkabal67'
-# login_user = 'kabal_m@itstep.academy'
-# login_user = 'Izmajlov_i'
-# password_user = 'mhB8nkA5559X'
+# login_user = ''
+# password_user = ''
+
 
 
